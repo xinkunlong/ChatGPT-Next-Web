0aefa734347eb470e45293d22463d1058ecab5f3
KuhnXin <kunlong.xin@outlook.com>
2024-03-23 14:52:58 +0800

    update title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Google Gemini
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
